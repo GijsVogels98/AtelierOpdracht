34f0f9b5cbbd141ea04e4c715c820dab95875dd7
GijsVogels98 <44775226+GijsVogels98@users.noreply.github.com>
2018-11-08 14:00:38 +0100

    register

--- application/controllers/users.php
+++ application/controllers/users.php
@@ -1,1 +1,28 @@
 <?php
+    class users extends CI_Controller{
+        public function register(){
+            $data['title'] = 'Registreer';
+
+            $this->form_validation->set_rules('username','Username','required');
+            $this->form_validation->set_rules('email','Email','required');
+            $this->form_validation->set_rules('email2','Herhaal Email','matches[email]');
+            $this->form_validation->set_rules('password','Wachtwoord','required');
+            $this->form_validation->set_rules('password2','Herhaal Wachtwoord','matches[password]');
+
+            if($this->form_validation->run() === FALSE){
+                $this->load->view('templates/header');
+                $this->load->view('users/register', $data);
+                $this->load->view('templates/footer');
+            } else{
+                //ecnrypt password
+                $enc_password = sha1($this->input->post('password'));
+
+                $this->user_model->register($enc_password);
+
+                //set messages
+                $this->session->set_flashdata('user_registered','Gebruiker Geregistreerd');
+
+                redirect('producten');
+            }
+        }
+    }
