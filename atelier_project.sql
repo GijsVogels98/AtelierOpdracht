787edc854be006c4216655e041f826964dc884c9
Stef <stef.verstraten@gmail.com>
2018-11-08 14:37:43 +0100

    Update atelier_project.sql

--- atelier_project.sql
+++ atelier_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Gegenereerd op: 08 nov 2018 om 12:52
+-- Gegenereerd op: 08 nov 2018 om 13:37
 -- Serverversie: 5.6.38
 -- PHP-versie: 7.2.1
 
@@ -30,7 +30,6 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `borrowed` (
   `id` int(11) NOT NULL,
-  `loan_slug` varchar(255) NOT NULL,
   `customer_name` varchar(255) NOT NULL,
   `customer_email` varchar(255) NOT NULL,
   `user_id` varchar(255) NOT NULL,
@@ -44,9 +43,10 @@ CREATE TABLE `borrowed` (
 -- Gegevens worden geëxporteerd voor tabel `borrowed`
 --
 
-INSERT INTO `borrowed` (`id`, `loan_slug`, `customer_name`, `customer_email`, `user_id`, `borrowed_at`, `borrowed_till`, `returned`, `product_id`) VALUES
-(1, 'stef-verstraten', 'Stef Verstraten', 's.verstraten@sintlucasedu.nl', '1', '2018-11-08 12:51:10', '2018-11-08 23:00:00', 'no', 24),
-(2, 'kees', 'Kees', 's.verstraten@sintlucasedu.nl', '1', '2018-11-08 12:51:48', '2018-11-16 23:00:00', 'no', 25);
+INSERT INTO `borrowed` (`id`, `customer_name`, `customer_email`, `user_id`, `borrowed_at`, `borrowed_till`, `returned`, `product_id`) VALUES
+(1, 'Stef Verstraten', 's.verstraten@sintlucasedu.nl', '1', '2018-11-08 13:22:57', '2018-11-08 23:00:00', 'yes', 24),
+(2, 'Kees', 's.verstraten@sintlucasedu.nl', '1', '2018-11-08 12:51:48', '2018-11-16 23:00:00', 'no', 25),
+(3, 'Gijs Vogels', 'g.vogels@gmail.com', '', '2018-11-08 13:17:33', '2018-11-09 23:00:00', 'no', 25);
 
 -- --------------------------------------------------------
 
@@ -81,16 +81,17 @@ CREATE TABLE `products` (
   `body` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `count` int(11) NOT NULL,
-  `available` int(11) NOT NULL
+  `product_lent` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Gegevens worden geëxporteerd voor tabel `products`
 --
 
-INSERT INTO `products` (`product_id`, `category_id`, `name`, `slug`, `body`, `created_at`, `count`, `available`) VALUES
-(24, 6, 'testtest', 'testtest', 'test', '2018-11-08 08:39:10', 0, 0),
-(25, 6, 'nummer 25', 'nummer-25', 'mooi', '2018-11-08 11:03:40', 10, 10);
+INSERT INTO `products` (`product_id`, `category_id`, `name`, `slug`, `body`, `created_at`, `count`, `product_lent`) VALUES
+(25, 6, 'nummer 25', 'nummer-25', 'mooi', '2018-11-08 11:03:40', 10, 10),
+(26, 6, 'test', 'test', 'test', '2018-11-08 13:30:08', 10, 10),
+(27, 6, 'Skon product', 'Skon-product', '', '2018-11-08 13:31:24', 15, 15);
 
 -- --------------------------------------------------------
 
@@ -142,7 +143,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT voor een tabel `borrowed`
 --
 ALTER TABLE `borrowed`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT voor een tabel `categories`
@@ -154,7 +155,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT voor een tabel `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT voor een tabel `users`
